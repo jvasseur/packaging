56243c048ef53712607e81ceafd9f8bb5709177a
Jérôme Vasseur <jerome.vasseur@jhome.fr>
2025-12-20 16:09:04 +0100

    Make GitHub Apps more customizable

--- jvasseur/packaging/app/github.py
+++ jvasseur/packaging/app/github.py
@@ -11,12 +11,24 @@ class GitHubApp(App):
     def repo():
         pass
 
+    def version(self, tag_name):
+        return tag_name.removeprefix('v')
+
     @abc.abstractmethod
     def assets(self, assets):
         pass
 
-    def version(self, tag_name):
-        return tag_name.removeprefix('v')
+    @abc.abstractmethod
+    def manifest_digest(self, data):
+        pass
+
+    @abc.abstractmethod
+    def retrieval(self, data):
+        pass
+
+    @abc.abstractmethod
+    def commands(self, data):
+        pass
 
     def implementations(self):
         releases = json.loads(urllib.request.urlopen(f'https://api.github.com/repos/{self.repo}/releases').read())
@@ -26,80 +38,73 @@ class GitHubApp(App):
 
             if version is not None:
                 for arch, asset in self.assets(release['assets']):
-                    yield f'{version}-{arch}', {
+                    yield version if arch is None else f'{version}-{arch}', {
                         'arch': arch,
                         'asset': asset,
                         'release': release,
                         'version': version,
                     }
 
-class ArchiveGitHubApp(GitHubApp):
-    @abc.abstractmethod
-    def commands(self, data):
-        pass
-
-    def extract(self, data):
-        return None
-
     def implementation(self, data):
-        extract = self.extract(data)
-
-        with Download(data['asset']['browser_download_url']) as archive:
-            digest = get_digest(archive.name, extract)
-
         return Implementation(
-            ManifestDigest(sha256new=digest),
-            Archive(
-                href=data['asset']['browser_download_url'],
-                size=data['asset']['size'],
-                extract=extract,
-            ),
+            ManifestDigest(sha256new=self.manifest_digest(data)),
+            self.retrieval(data),
             *self.commands(data),
             arch=data['arch'],
-            id=f'{data['version']}-{data['arch']}',
+            id=data['version'] if data['arch'] is None else f'{data['version']}-{data['arch']}',
             version=data['version'],
             released=data['release']['published_at'][0:10],
             stability='testing' if data['release']['prerelease'] else 'stable',
+        )
+
+class ArchiveGitHubApp(GitHubApp):
+    def extract(self, data):
+        return None
+
+    def manifest_digest(self, data):
+        extract = self.extract(data)
+
+        with Download(data['asset']['browser_download_url']) as archive:
+            return get_digest(archive.name, extract)
+
+    def retrieval(self, data):
+        return Archive(
+            href=data['asset']['browser_download_url'],
+            size=data['asset']['size'],
+            extract=self.extract(data),
         )
 
 class FileGitHubApp(GitHubApp):
     def file_name(self, data):
         return posixpath.basename(urllib.parse.urlsplit(data['asset']['browser_download_url']).path)
 
-    def implementation(self, data):
-        file_name = self.file_name(data)
+    def file_executable(self, data):
+        return True
 
+    def manifest_digest(self, data):
         if data['asset']['digest'] is not None and data['asset']['digest'].startswith('sha256:'):
             sha256 = data['asset']['digest'].removeprefix('sha256:')
         else:
             with Download(data['asset']['browser_download_url']) as file:
                 sha256 = hashlib.file_digest(file, 'sha256').hexdigest()
 
-        digest = get_manifest_digest([
+        return get_manifest_digest([
             ManifestFile(
-                executable=True,
-                mtime= 0,
-                name=file_name,
+                executable=self.file_executable(data),
+                mtime=0,
+                name=self.file_name(data),
                 sha256=sha256,
                 size=data['asset']['size'],
             ),
         ], 'sha256new')
 
-        return Implementation(
-            ManifestDigest(sha256new=digest),
-            File(
-                href=data['asset']['browser_download_url'],
-                size=data['asset']['size'],
-                dest=file_name,
-                executable=True,
-            ),
-            Command(
-                name='run',
-                path=file_name,
-            ),
-            arch=data['arch'],
-            id=f'{data['version']}-{data['arch']}',
-            version=data['version'],
-            released=data['release']['published_at'][0:10],
-            stability='testing' if data['release']['prerelease'] else 'stable',
+    def retrieval(self, data):
+        return File(
+            href=data['asset']['browser_download_url'],
+            size=data['asset']['size'],
+            dest=self.file_name(data),
+            executable=self.file_executable(data),
         )
+
+    def commands(self, data):
+        yield Command(name='run', path=self.file_name(data))
